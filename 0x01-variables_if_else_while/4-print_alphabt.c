53883f9bcd9698dca8fde15808f64ff44b709cf7
David-VargasV <4523@holbertonschool.com>
2022-02-11 14:37:07 -0800

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - print alphabet
+* Return: 0
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'q' && letter != 'e')
+putchar (letter);
+}
+putchar ('\n');
+return (0);
+}
