57eb12ddb09f604029d66b198d37c1a91bc428c0
David-VargasV <4523@holbertonschool.com>
2022-02-11 12:25:17 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,10 +12,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if ((n % 10) > 5)
-printf("%d Last digit of n and is greater than 5\n")
+printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 if ((n % 10) == 0)
-printf("%d Last digit of n and is 0\n")
+printf("Last digit of %d is %d and is 0\n", n, n % 10);
 if ((n % 10) < 6 && ((n % 10) != 0)
-printf("%d Last digit of and is less than 6 and not 0\n")
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 return (0);
 }
