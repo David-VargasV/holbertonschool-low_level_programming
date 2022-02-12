22a93b650c2f4908f75988b4a2d7d44bf1581677
David-VargasV <4523@holbertonschool.com>
2022-02-11 17:54:16 -0800

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - print number
+* Return: 0
+*/
+int main(void)
+{
+int num;
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+if (num != '9')
+putchar(',');
+putchar(' ')
+}
+putchar('\n');
+return (0);
+}
