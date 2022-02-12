c6b3f35e4d2fa16eb7691fcf82ca6476b1b12219
David-VargasV <4523@holbertonschool.com>
2022-02-11 16:58:37 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - print number
+* Return: 0
+*/
+int main(void)
+{
+int num;
+char letter;
+for (num = '0'; num <= '9'; num++)
+putchar(num);
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter)
+putchar('\n');
+return (0);
+}
