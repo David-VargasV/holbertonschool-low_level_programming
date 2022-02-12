4071961d96a0e96800a3225aefb3be051462cfd3
David-VargasV <4523@holbertonschool.com>
2022-02-11 16:07:52 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - print alphabet
+* Return: 0
+*/
+int main(void)
+{
+char letter;
+for (letter = 'z'; letter >= 'a'; letter--)
+putchar (letter);
+putchar ('\n');
+return (0);
+}
