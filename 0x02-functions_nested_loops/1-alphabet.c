b4e3aa1d3a1df9d52992c2200aa5d50e3483b0ad
David-VargasV <4523@holbertonschool.com>
2022-02-15 11:52:08 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_alphabet - print alphabet
+* Return: 0
+*/
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar (letter);
+putchar ('\n');
+}
