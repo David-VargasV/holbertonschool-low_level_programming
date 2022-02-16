9e30df24679699431f829598401a6b5f56a336d1
David-VargasV <4523@holbertonschool.com>
2022-02-16 09:02:25 -0800

    print 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,18 +8,16 @@
 
 void print_to_98(int n)
 {
-int num = 98;
-while (n != 98)
+while (n < 98)
 {
-_putchar(n);
-_putchar(',');
-_putchar(' ');
-if (n > 98)
-n--;
-else
+printf("%d, ", n);
 n++;
 }
-_putchar((num / 10) + '0');
-_putchar((num % 10) + '0');
-_putchar('\n');
+if (n > 98)
+{
+printf("%d, ", n);
+n--;
+}
+else if (n == 98)
+printf("%d\n", n);
 }
