f5b6267b8fb63184e921f61bee903fd33b0ac4e5
David-VargasV <4523@holbertonschool.com>
2022-02-16 08:39:17 -0800

    print to up 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_to_98 - print up to 98
+* @num: var
+* @n: var
+* Return: 0
+*/
+
+void print_to_98(int n)
+{
+int num = 98;
+while (n != 98)
+{
+_putchar('n');
+_putchar(',');
+_putchar(' ');
+if (n > 98)
+n--;
+else
+n++;
+}
+_putchar((num / 10) + '0');
+_putchar((num % 10) + '0');
+_putchar('\n');
+}
