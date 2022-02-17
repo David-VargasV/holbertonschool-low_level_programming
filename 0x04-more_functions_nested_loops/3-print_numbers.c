703d155bd59cdc5db70c0d4012c156060a4487cc
David-VargasV <4523@holbertonschool.com>
2022-02-17 11:03:45 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* print_numbers - print alphabet
+* print_numbers - print numbers
 * Return: 0
 */
 void print_numbers(void)
 {
 char num;
 for (num >= '0'; num <= '9'; num++)
-putchar (num);
-putchar ('\n');
+_putchar (num);
+_putchar ('\n');
 }
