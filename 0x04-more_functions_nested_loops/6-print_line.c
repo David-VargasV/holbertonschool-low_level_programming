4ee4ebec30477d5ab01c7ceec34978c740b79c8f
David-VargasV <4523@holbertonschool.com>
2022-02-17 12:16:01 -0800

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_line - print numbers
+* @n: number of time
+* Return: 0
+*/
+void print_line(int n)
+{
+while (n > 0)
+{
+_putchar ('_');
+n--;
+}
+_putchar ('\n');
+}
