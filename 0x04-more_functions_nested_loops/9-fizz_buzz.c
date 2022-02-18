98550ade13df6d9f4fc2424925173f2efc0644e9
David-VargasV <4523@holbertonschool.com>
2022-02-17 18:49:00 -0800

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - print multiples
+* Return: 0
+*/
+
+int main(void)
+{
+int x;
+
+for (x = 1; x < 100; x++)
+if (x % 15 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (x % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (x % 3 == 0)
+{
+printf("Fizz ");
+}
+else
+{
+printf("%d ", x);
+}
+printf("Buzz ");
+printf("\n");
+return (0);
+}
