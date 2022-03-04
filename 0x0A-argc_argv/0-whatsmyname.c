4f2e331303c7e8be000543141fb25ec2c8750339
David-VargasV <4523@holbertonschool.com>
2022-03-04 09:31:02 -0800

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - print the name
+* @argc: size of argv
+* @argv: program
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+printf("%s\n", argv[0]);
+
+return (0);
+}
