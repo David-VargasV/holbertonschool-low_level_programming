43a7df05602ef0e98df9278a96e506fbc53d919e
David-VargasV <4523@holbertonschool.com>
2022-03-17 09:13:13 -0700

    print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+* print_name - print the name
+* @name: name
+* @f: pointer
+* Return: 0 NULL pointer
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+if (f)
+{
+(*f)(name);
+}
+}
