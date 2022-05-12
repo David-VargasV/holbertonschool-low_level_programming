037712989fd439e3106b1e2a8d0bfe81a2dc4b72
David-vargasV <4523@holbertonschool.com>
2022-05-12 15:45:38 -0500

    sum_dlistint

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,9 +1,19 @@
 #include "lists.h"
 
 /**
- * 
- * 
- * 
+ * sum_dlistint - function that returns the sum of all the data
+ * @head: head
+ * Return: Value or 0
  */
 
 int sum_dlistint(dlistint_t *head)
+{
+int add = 0;
+
+while (head)
+{
+add += head->n;
+head = head->next;
+}
+return (add);
+}
