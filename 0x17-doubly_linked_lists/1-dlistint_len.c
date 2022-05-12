2baa26bd6dcbda24adfcfd451bb8572d0291e2a6
David-vargasV <4523@holbertonschool.com>
2022-05-12 13:17:01 -0500

    dlistint_len

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,19 @@
 #include "lists.h"
 
 /**
- * 
- * 
- * 
+ * dlistint_len - returns the number of elements
+ * @h: head
+ * Return: nodes
  */
 
 size_t dlistint_len(const dlistint_t *h)
+{
+size_t nod = 0;
+
+while (h)
+{
+nod++;
+h = h->next;
+}
+return (nod);
+}
