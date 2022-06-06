fc000066311b5636c2883b6452ceb585487e694a
David-vargasV <4523@holbertonschool.com>
2022-06-06 14:35:33 -0500

    h_t_get

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function that retrieves a value associated with a key
+ * @ht:  is the hash table you want to look into
+ * @key:  is the key you are looking for
+ * Return: Value or NULL
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *tmp;
+	unsigned int index;
+
+	if (ht == NULL)
+		return (NULL);
+	{
+		index = key_index((const unsigned char *)key, ht->size);
+
+		if (ht->array[index])
+		{
+			tmp = ht->array[index];
+			if (tmp)
+			{
+				if (strcmp(tmp->key, (char *)key) == 0)
+					return (tmp->value);
+				tmp = tmp->next;
+			}
+		}
+	}
+	return (NULL);
+}
