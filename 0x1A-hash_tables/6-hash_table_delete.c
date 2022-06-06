4bd0af6c1cb84827a532274416dbfa017b5a4b67
David-vargasV <4523@holbertonschool.com>
2022-06-06 15:39:12 -0500

    h_t_delete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - function that deletes a hash table
+ * @ht:  is the hash table
+ * Return: Value
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	unsigned int x;
+
+	if (ht == NULL)
+		return;
+
+	for (x = 0; x < ht->size; x++)
+	{
+		if (ht->array[x] != NULL)
+		{
+			tmp = ht->array[x]->next;
+			free(ht->array[x]->key);
+			free(ht->array[x]->value);
+			free(ht->array[x]);
+			ht->array[x] = tmp;
+		}
+		free(ht->array[x]);
+	}
+	free(ht->array);
+	free(ht);
+}
