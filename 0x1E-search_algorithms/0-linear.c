b7c52b1b555af0ea20f2231db6c8f52236106472
David-vargasV <4523@holbertonschool.com>
2022-08-08 22:13:56 -0500

    Linear_search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,4 +11,18 @@
  * Return: Value or -1
  */
 
-int linear_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value)
+{
+	size_t x;
+
+	if (array == NULL)
+		return (-1);
+
+	for (x = 0; x < size; x++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", x, array[x]);
+		if (array[x] == value)
+			return (x);
+	}
+	return (-1);
+}
